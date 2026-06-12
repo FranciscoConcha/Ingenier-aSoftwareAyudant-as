2723f9bec1b859d3e52a0f06f483ccf75f592422
FranciscoConcha <Francisco.concha.urquieta@gmail.com>
2026-06-11 23:56:09 -0400

    feat: Dto para procesar un pago

---
+++ Src/Dtos/Payment/ProcessPaymentDto.cs
@@ -0,0 +1,17 @@
+namespace ProyectoDivine.Src.Dtos.Payment;
+
+public class ProcessPaymentData
+{
+    //Datos para la reserva
+    public int UserId{get;set;}
+    public string ReservationCode{get;set;} = string.Empty;
+    /// Si se procesa directamente el págo al crear la reserva se puede agregar aquí pero no es necesario
+    public int? Amount {get;set;}
+    // Datos de la tarjeta
+    public string CardNum{get;set;} =string.Empty;
+    public string SecurityCode{get;set;} =string.Empty;
+    public string TableExpiration {get;set;} = string.Empty;
+    public string AgeExpiration {get;set;} = string.Empty;
+    public string NameHolder {get;set;} =string.Empty;
+
+}
