4e6548e8a5ed90e99cf11f20de6533a4f1fd8859
FranciscoConcha <Francisco.concha.urquieta@gmail.com>
2026-06-12 02:39:05 -0400

    chore: Cambios pequeños

---
+++ Src/Db/Migrations/20260612045337_PaymentDataUpdate.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ProyectoDivine.src.Db.Migrations
+{
+    /// <inheritdoc />
+    public partial class PaymentDataUpdate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "DateTransaccion",
+                table: "Payments",
+                type: "timestamp with time zone",
+                nullable: false,
+                oldClrType: typeof(DateOnly),
+                oldType: "date");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateOnly>(
+                name: "DateTransaccion",
+                table: "Payments",
+                type: "date",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp with time zone");
+        }
+    }
+}
