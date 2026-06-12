bca850dbe3b5960ae54a78898231e92ad6960589
FranciscoConcha <Francisco.concha.urquieta@gmail.com>
2026-06-12 01:19:46 -0400

    chore: Se quita usuario id dado que viene del la cookei

--- Src/Dtos/Payment/ProcessPaymentDto.cs
+++ Src/Dtos/Payment/ProcessPaymentDto.cs
@@ -3,7 +3,7 @@ namespace ProyectoDivine.Src.Dtos.Payment;
 public class ProcessPaymentData
 {
     //Datos para la reserva
-    public int UserId{get;set;}
+    
     public string ReservationCode{get;set;} = string.Empty;
     /// Si se procesa directamente el págo al crear la reserva se puede agregar aquí pero no es necesario
     public int? Amount {get;set;}
